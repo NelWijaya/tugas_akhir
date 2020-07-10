ff03095849407f4e69f00345d9af1f7c9380a27e
NelWijaya <67580104+NelWijaya@users.noreply.github.com>
2020-07-10 16:19:11 +0700

    Penambahan Form Add Question
    penambahan menggunakan modal

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,33 @@
 @section('content')
 <div class="row">
     <div class="col-12 col-md-3">
-        <a href="#" class="btn btn-outline-success mb-3">Add Question</a>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-outline-success mb-3" data-toggle="modal" data-target="#addquestion">
+            Add Question
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="addquestion" tabindex="-1" role="dialog" aria-labelledby="addquestionLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addquestionLabel">Add Question</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="#" method="post">
+                        <div class="modal-body">
+                            <input type="text" class=" mb-2" placeholder="Title" required>
+                            <textarea name="content" class="form-control my-editor" id="answer" rows="10" style="width: 100%;"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Add Question</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-12 col-md-9">
         <form class="form-inline my-2 my-lg-0" method="POST" action="#">
@@ -56,3 +82,42 @@
   </div>
 </div>
 @endsection
+@push('script')
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+<script>
+  var editor_config = {
+    path_absolute : "/",
+    selector: "textarea.my-editor",
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
+</script>
+@endpush
